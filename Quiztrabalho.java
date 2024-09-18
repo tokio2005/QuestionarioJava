c457049a9ea66022a87ebe3341d0b5c6e376b581
tokio2005 <chrystiantokio152@gmail.com>
2024-09-17 21:09:58 -0300

    primeiro commit

--- Quiztrabalho.java
+++ Quiztrabalho.java
@@ -57,6 +57,13 @@ public class Questao {
 
 public class Quiztrabalho {
     public static void main(String[] args) {
+        public static void main(String[] args) {
+            System.out.println("************");
+            System.out.println("Faculdade: Centro Universitário Alfredo Nasser");
+            System.out.println("Aluno:Chrystian Alexandre Cardoso Tokio");
+            System.out.println("Professor:Brenno Pimenta");
+            System.out.println("************");
+            System.out.println("Bem-vindo ao Quiz de Conhecimentos Gerais");
 
     }
 
